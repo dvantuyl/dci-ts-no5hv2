618779a0d4c52c6a3f9caa4cf1797e612c0e03fb
dvantuyl <dvantuyl@gmail.com>
2023-07-16 12:35:17 -0700

    Update to dot syntax

--- src/HelloWorld.ts
+++ src/HelloWorld.ts
@@ -2,17 +2,34 @@
  * @DCI-context
  * A speaker proclaims a phrase to the world, that dutifully notes it
  */
-export function HelloWorld(
-  Speaker: { phrase: string },
-  World: { log: (msg: unknown) => void }
-) {
-  function Speaker_proclaim() {
-    World_note(Speaker.phrase);
-  }
 
-  function World_note(phrase: string) {
-    World.log(phrase);
-  }
 
-  Speaker_proclaim();
+interface Speaker {
+  phrase: string,
+  proclaim?: () => void
 }
+
+interface World {
+  log: (msg: unknown) => void,
+  note?: (phrase: string) => void
+}
+
+export function HelloWorld(
+  Speaker: Speaker,
+  World: World
+) {
+
+  World.note = (phrase: string) => World.log(phrase);
+  Speaker.proclaim = () => World.note!(Speaker.phrase!)
+
+  return {
+    say: () => Speaker.proclaim!()
+  }
+}
+
+const you = { phrase: "Hello Dwight!"}
+
+const world = HelloWorld(you, console)
+
+world.say()
+
